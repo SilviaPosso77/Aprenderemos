947ab211d9bcb477c50a037af06036311228a5a8
SILVI COMPUWORK <silvia.posso@est.iudigital.edu.co>
2024-10-18 23:46:34 -0500

    Subiendo aprederemos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola, estoy subiendo el proyecto de N°1." +
+                "Tiene como nombre °Escribe un programa que muestre tu nombre en pantalla!");
     }
 }
