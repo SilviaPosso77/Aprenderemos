e8c9ee2e25ed22339a95f18f041dcce7e7560e1f
SILVI COMPUWORK <silvia.posso@est.iudigital.edu.co>
2024-10-19 01:00:53 -0500

    Subiendo modificación Ejercicio 1,2 y 3

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,25 @@ public class Main {
                 "Tiene como nombre °Escribe un programa que muestre tu nombre en pantalla°!");
         System.out.println("Soy estudiente de la tecnologia en desarrollo de software");
         System.out.println("Vivo en el municipio de bello-antioquia");
-        System.out.printf("Mi telefono es:3046142497NOOOOO\n");
+        System.out.printf("Mi telefono es:3046142497\n" +
+                "\n"+
+                "\n");
+
+    System.out.printf("Esto me traducira la palabra en ingles a español\n"+
+            "%-10s %s\n", "1: computer", "ordenador");
+        System.out.printf("%-10s%s\n","2: student","estudiante");
+        System.out.printf("%-10s%s\n","3: cat","gato");
+        System.out.printf("%-10s%s\n","4: book","libro");
+        System.out.printf("%-10s%s\n","5: science","ciencia");
+        System.out.printf("%-10s%s\n","6: nature","naturaleza");
+        System.out.printf("%-10s%s\n","7: ocean","océano");
+        System.out.printf("%-10s%s\n","8: music","música");
+
+
     }
 }
+
+
+
 
 
